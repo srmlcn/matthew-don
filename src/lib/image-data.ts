7066238707ff1a6c437e4150e17a6f2f79f2cbdd
Spencer Cain <cainspencerm@protonmail.com>
2025-02-10 17:59:05 -0500

    feat: add image data for book display cards

---
+++ src/lib/image-data.ts
@@ -0,0 +1,34 @@
+type ImageData = {
+  src: string
+  alt: string
+  width: number
+  height: number
+}
+
+export const adventuresOfLucaAndKaiTheMoonQueenImageData: ImageData = {
+  src: "/adventures/book1-cover.jpg",
+  alt: "The Adventures of Luca and Kai: The Moon Queen",
+  width: 1169,
+  height: 1749,
+}
+
+export const adventuresOfLucaAndKaiBook2of3ImageData: ImageData = {
+  src: "/adventures/book2-cover.png",
+  alt: "The Adventures of Luca and Kai (Book 2 of 3)",
+  width: 1047,
+  height: 1571,
+}
+
+export const adventuresOfLucaAndKaiBook3of3ImageData: ImageData = {
+  src: "/adventures/book3-cover.png",
+  alt: "The Adventures of Luca and Kai (Book 3 of 3)",
+  width: 1271,
+  height: 1906,
+}
+
+export const celebrationOfTheHistoryOfCelebratingHistoryImageData: ImageData = {
+  src: "/celebration/cover.jpg",
+  alt: "A Celebration of the History of Celebrating History",
+  width: 1200,
+  height: 1920,
+}
