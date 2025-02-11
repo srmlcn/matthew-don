958b9ade05c6bd35c6356af09a0133711ce2942e
Spencer Cain <cainspencerm@protonmail.com>
2025-02-10 22:17:06 -0500

    refactor: make home page invitation responsive

--- src/app/components/invitation.tsx
+++ src/app/components/invitation.tsx
@@ -6,8 +6,15 @@ import Link from "next/link"
 export function Invitation() {
   return (
     <div className="flex flex-col items-center gap-8">
-      <p className="font-bold text-4xl">{"Wanna know a little about me?"}</p>
-      <Button as={Link} href="/about" color="primary" className="text-xl h-12">
+      <p className="font-bold text-4xl text-center">
+        {"Wanna know a little about me?"}
+      </p>
+      <Button
+        as={Link}
+        href="/about"
+        color="primary"
+        className="text-xl h-full px-4 py-2 text-wrap w-48 sm:w-fit text-center"
+      >
         {"I dunno, maybe click this button here then."}
       </Button>
     </div>
