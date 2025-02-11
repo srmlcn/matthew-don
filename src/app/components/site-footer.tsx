b14810618a00bfc01525c9e715271981067f05d2
Spencer Cain <cainspencerm@protonmail.com>
2025-02-11 13:42:06 -0500

    feat: create a site footer

---
+++ src/app/components/site-footer.tsx
@@ -0,0 +1,7 @@
+export function SiteFooter() {
+  return (
+    <footer className="flex p-6 items-center justify-center">
+      <p>© 2025 Matthew Don. All rights reserved.</p>
+    </footer>
+  )
+}
