7487b859de8bff25ce763188cd1521b0f397bde9
Spencer Cain <cainspencerm@protonmail.com>
2025-02-10 20:44:48 -0500

    feat: add metadata to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,54 @@ import {
   adventuresOfLucaAndKaiTheMoonQueenLinkData,
   celebrationOfTheHistoryOfCelebratingHistoryLinkData,
 } from "@/lib/link-data"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Home | Matthew Don",
+  description:
+    "Discover the works of Matthew Don, including the thrilling adventure 'The Adventures of Luca and Kai: The Moon Queen' and the satirical comedy 'A Celebration of the History of Celebrating History.' Explore stories that range from fun-filled journeys for all ages to irreverent humor for mature readers.",
+  keywords: [
+    "fiction books",
+    "adventure books",
+    "humor books",
+    "The Adventures of Luca and Kai",
+    "The Moon Queen",
+    "A Celebration of the History of Celebrating History",
+    "author",
+    "new books",
+    "book series",
+    "upcoming books",
+  ],
+  openGraph: {
+    title: "Home | Matthew Don",
+    description:
+      "Discover the works of Matthew Don, including the thrilling adventure 'The Adventures of Luca and Kai: The Moon Queen' and the satirical comedy 'A Celebration of the History of Celebrating History.' Explore stories that range from fun-filled journeys for all ages to irreverent humor for mature readers.",
+    images: [
+      {
+        url: "/matthew-don.jpg",
+        alt: "Matthew Don",
+        width: 724,
+        height: 763,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@MatthewDon",
+    title: "Home | Matthew Don",
+    description:
+      "Discover the works of Matthew Don, including the thrilling adventure 'The Adventures of Luca and Kai: The Moon Queen' and the satirical comedy 'A Celebration of the History of Celebrating History.' Explore stories that range from fun-filled journeys for all ages to irreverent humor for mature readers.",
+    creator: "@MatthewDon",
+    images: [
+      {
+        url: "/matthew-don.jpg",
+        alt: "Matthew Don",
+        width: 724,
+        height: 763,
+      },
+    ],
+  },
+}
 
 export default function Home() {
   return (
