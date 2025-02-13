353be68a401714fda638429af62a8455c3f50e07
Spencer Cain <cainspencerm@protonmail.com>
2025-02-13 09:49:14 -0500

    refactor: match attribute order of BookPageProps for adventures book

--- src/app/books/the-adventures-of-luca-and-kai-the-moon-queen/page.tsx
+++ src/app/books/the-adventures-of-luca-and-kai-the-moon-queen/page.tsx
@@ -11,12 +11,12 @@ const props: BookPageProps = {
   descriptions: [
     "Luca and Kai are two young brothers with an openness for adventure that's as big as their imagination. At least what their imagination can allow while grounded in their room. When two portals open next to them, they are thrust into an experience like no other, meeting unique friends and fearsome enemies along the way. Through fast paced action, hilarious antics and mind-bending physics, our heroes discover they are in control of their own destinies. Who will they decide to become?",
   ],
-  image: imageData,
-  link: linkData,
-  images: imagesData,
-  reviews: reviewsData,
   secondaryDescription:
     "Available now in paperback, Ebook, and FREE through Kindle Unlimited!",
+  image: imageData,
+  images: imagesData,
+  link: linkData,
+  reviews: reviewsData,
 }
 
 export default function Page() {
