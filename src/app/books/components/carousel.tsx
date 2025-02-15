8d3a39338b870bb80d14a24186e23147d02023a3
Spencer Cain <cainspencerm@protonmail.com>
2025-02-15 17:40:05 -0500

    fix: prevent relative and absolute class collision with surrounding components

--- src/app/books/components/carousel.tsx
+++ src/app/books/components/carousel.tsx
@@ -75,64 +75,59 @@ export function Carousel({ images }: { images: ImageData[] }) {
   }
 
   return (
-    <div
-      className="relative w-full overflow-hidden"
-      ref={containerRef}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+    <div className="mx-auto w-full max-w-[32rem]">
       <div
-        className={`flex ${
-          !isDragging ? "transition-transform duration-300 ease-out" : ""
-        }`}
-        style={{
-          transform: `translateX(${
-            -currentIndex * containerWidth + effectiveDrag
-          }px)`,
-        }}
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
+        className="relative w-full overflow-hidden"
+        ref={containerRef}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
-        {images.map(({ src, alt, width, height }, index) => (
-          <div key={index} className="flex-shrink-0 w-full">
-            <Image
-              src={src}
-              alt={alt}
-              width={width}
-              height={height}
-              className="object-cover w-full"
-            />
-          </div>
-        ))}
-      </div>
+        <div
+          className={`flex ${!isDragging ? 'transition-transform duration-300 ease-out' : ''}`}
+          style={{
+            transform: `translateX(${-currentIndex * containerWidth + effectiveDrag}px)`,
+          }}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+        >
+          {images.map(({ src, alt, width, height }, index) => (
+            <div key={index} className="flex-shrink-0 w-full">
+              <Image
+                src={src}
+                alt={alt}
+                width={width}
+                height={height}
+                className="object-cover w-full"
+              />
+            </div>
+          ))}
+        </div>
 
-      <button
-        onClick={handlePrev}
-        disabled={currentIndex === 0}
-        className={`absolute z-10 top-1/2 left-2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full transition-opacity duration-300 ${
-          isHovered ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-      >
-        Prev
-      </button>
-      <button
-        onClick={handleNext}
-        disabled={currentIndex === images.length - 1}
-        className={`absolute z-10 top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full transition-opacity duration-300 ${
-          isHovered ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-      >
-        Next
-      </button>
+        <button
+          onClick={handlePrev}
+          disabled={currentIndex === 0}
+          className={`absolute z-10 top-1/2 left-2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-0 pointer-events-none'
+            }`}
+        >
+          Prev
+        </button>
+        <button
+          onClick={handleNext}
+          disabled={currentIndex === images.length - 1}
+          className={`absolute z-10 top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-0 pointer-events-none'
+            }`}
+        >
+          Next
+        </button>
+      </div>
 
       <div className="flex justify-center space-x-2 mt-4">
         {images.map((_, index) => (
           <div
             key={index}
-            className={`w-2 h-2 rounded-full transition-colors duration-300 ${
-              index === currentIndex ? "bg-gray-800" : "bg-gray-400"
-            }`}
+            className={`w-2 h-2 rounded-full transition-colors duration-300 ${index === currentIndex ? 'bg-gray-800' : 'bg-gray-400'
+              }`}
           />
         ))}
       </div>
