2d9bc0669b3e66ce99976f2e6f32cfbcf77732db
Spencer Cain <cainspencerm@protonmail.com>
2025-02-16 15:32:30 -0500

    feat: add amazon icon to amazon links on the book pages

--- src/app/books/components/book-page.tsx
+++ src/app/books/components/book-page.tsx
@@ -8,6 +8,7 @@ import { Button } from "@heroui/react"
 import Image from "next/image"
 import Link from "next/link"
 import { Carousel } from "./carousel"
+import { FaAmazon } from "react-icons/fa"
 
 export type BookPageProps = {
   title: string
@@ -72,6 +73,7 @@ export function BookPage({
         href={linkData.external.href}
         target="_blank"
         rel="noopener noreferrer"
+        startContent={<FaAmazon className="text-lg" />}
       >
         {linkData.external.label}
       </Button>
