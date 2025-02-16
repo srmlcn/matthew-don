b543cffff29b92cbc9b8d1ce13d8be13c71c0d7f
Spencer Cain <cainspencerm@protonmail.com>
2025-02-16 16:22:33 -0500

    refactor: get button link styles using "getVendorStyle" on the book pages

--- src/app/books/components/book-page.tsx
+++ src/app/books/components/book-page.tsx
@@ -3,13 +3,12 @@
 import type { ImageData } from "@/lib/image-data"
 import type { LinkData } from "@/lib/link-data"
 import type { ReviewData } from "@/lib/review-data"
+import { getVendorStyle } from "@/lib/vendor-styles"
 import { StarIcon } from "@heroicons/react/24/solid"
 import { Button } from "@heroui/react"
 import Image from "next/image"
 import Link from "next/link"
 import { Carousel } from "./carousel"
-import { FaAmazon } from "react-icons/fa"
-import { ReactNode } from "react"
 
 export type BookPageProps = {
   title: string
@@ -70,18 +69,7 @@ export function BookPage({
       </div>
       <div className="flex items-center">
         {linkData.external.map((link) => {
-          let background: string
-          let icon: ReactNode | undefined
-
-          switch (link.vendor) {
-            case "amazon":
-              background = "bg-[#febd69]"
-              icon = <FaAmazon className="text-lg" />
-              break
-            default:
-              background = ""
-              icon = undefined
-          }
+          const { background, icon } = getVendorStyle(link.vendor)
 
           return (<Button
             as={Link}
