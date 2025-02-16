817422e444d5abc4fdf9394c8f6d6b9c01c8f31a
Spencer Cain <cainspencerm@protonmail.com>
2025-02-16 15:49:19 -0500

    feat: update link data with link vendors

--- src/lib/link-data.ts
+++ src/lib/link-data.ts
@@ -10,34 +10,38 @@ export type LinkData = {
 }
 
 export const adventuresOfLucaAndKaiTheMoonQueenLinkData: LinkData = {
-  external: {
+  external: [{
+    vendor: "amazon",
     href: "https://www.amazon.com/Adventures-Luca-Kai-Moon-Queen/dp/B0D8BGZWNG/ref=sr_1_1?crid=32XVBESZWY8XY&dib=eyJ2IjoiMSJ9.5kREcehvyjzCUnPVvAVzezHkbkoApLN3dbZvZ2PtLvrRg5B8S0i8Fcd4y01pCFbi5NP5uZElRzNBwzzpIg_U0SjGYGaLUvi5m_MEUZwq0fRsyetbZgh5AJ4eRjROJnuTpBPfBuQWCpDFDgam2G0bdYjLjNpQ9Ng7zXfZEQZxbCySR7tNELmMNYpsKsEQeIXeW-oUWcyG9atztMPmWtI4bOMuqunicRfafPVCQZOnyAg.sR81PuCIdQRkAZht0_uKFNbQECAGGXm10Eb9EpaMYJY&dib_tag=se&keywords=adventures+of+luca+and+kai&qid=1737246838&sprefix=adventures+of+luca+and+kai%2Caps%2C129&sr=8-1",
     label: "Check out on Amazon!",
-  },
+  }],
   internal: {
     href: "/books/the-adventures-of-luca-and-kai-the-moon-queen",
   },
 }
 
 export const adventuresOfLucaAndKaiBook2of3LinkData: LinkData = {
-  external: {
+  external: [{
+    vendor: "amazon",
     href: "https://www.amazon.com/stores/Matthew-Don/author/B00YF2KSWO?ref=ap_rdr&isDramIntegrated=true&shoppingPortalEnabled=true",
     label: "Follow on Amazon",
-  },
+  }],
 }
 
 export const adventuresOfLucaAndKaiBook3of3LinkData: LinkData = {
-  external: {
+  external: [{
+    vendor: "amazon",
     href: "https://www.amazon.com/stores/Matthew-Don/author/B00YF2KSWO?ref=ap_rdr&isDramIntegrated=true&shoppingPortalEnabled=true",
     label: "Follow on Amazon",
-  },
+  }],
 }
 
 export const celebrationOfTheHistoryOfCelebratingHistoryLinkData: LinkData = {
-  external: {
+  external: [{
+    vendor: "amazon",
     href: "https://www.amazon.com/Celebration-History-Celebrating-ebook/dp/B00YEWEHD6/ref=sr_1_1?crid=N78CCUOB5CWL&dib=eyJ2IjoiMSJ9.LSQAmVU9AyzSJfDf8FtIWGSLbaXOj99jaj7q5_x2AoFl0Wy-fHsk0AW36AyRpYEXCtI5vDMv3StvyrhCLB0Hfe0rU7lBq-yLAgXxBN0SzCDjcmt3Wul0KCv0p2g92jVcaS8Sp1PbY88g7PpJJIyvXBvJV4T3NVnQGdpYLEMfmqqumZfF-JRbPlO6dX6DEY9bbENqTOSPkyhCdH9pszKQFg.EKDZRgqw2l-09531iaBi20q6QOgBskRkt6GMNOxtyL8&dib_tag=se&keywords=a+celebration+of+the+history+of+celebrating+history&qid=1737247039&sprefix=a+celebration+of+the+history+of+celebrating+history%2Caps%2C129&sr=8-1",
     label: "Check out on Amazon!",
-  },
+  }],
   internal: {
     href: "/books/a-celebration-of-the-history-of-celebrating-history",
   },
