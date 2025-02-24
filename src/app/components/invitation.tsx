5759c33818dabafa4e170d2a9d51232ca50dc907
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 13:32:16 -0500

    refactor: remove curly braces around static text in the invitation component

--- src/app/components/invitation.tsx
+++ src/app/components/invitation.tsx
@@ -7,7 +7,7 @@ export function Invitation() {
   return (
     <div className="flex flex-col items-center gap-8">
       <p className="font-bold text-4xl text-center">
-        {"Wanna know a little about me?"}
+        Wanna know a little about me?
       </p>
       <Button
         as={Link}
@@ -15,7 +15,7 @@ export function Invitation() {
         color="primary"
         className="text-xl h-full px-4 py-2 text-wrap w-48 sm:w-fit text-center"
       >
-        {"I dunno, maybe click this button here then."}
+        I dunno, maybe click this button here then.
       </Button>
     </div>
   )
