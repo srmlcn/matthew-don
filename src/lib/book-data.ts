f700b4ec1a3bf50ab2426fc689803fd0a87e2ee9
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 14:14:28 -0500

    refactor: move the current books to the book data file

--- src/lib/book-data.ts
+++ src/lib/book-data.ts
@@ -1,5 +1,13 @@
-import type { ImageData } from "./image-data"
-import type { LinkData } from "./link-data"
+import {
+  adventuresOfLucaAndKaiTheMoonQueenImageData,
+  celebrationOfTheHistoryOfCelebratingHistoryImageData,
+  type ImageData,
+} from "./image-data"
+import {
+  adventuresOfLucaAndKaiTheMoonQueenLinkData,
+  celebrationOfTheHistoryOfCelebratingHistoryLinkData,
+  type LinkData,
+} from "./link-data"
 
 export type BookData = {
   title: string
@@ -7,3 +15,28 @@ export type BookData = {
   imageData: ImageData
   linkData: LinkData
 }
+
+export const adventuresOfLucaAndKaiBook1of3BookData: BookData = {
+  title: "The Adventures of Luca and Kai: The Moon Queen (Book 1 of 3)",
+  descriptions: [
+    "Fun for all ages!",
+    "Available in paperback, Ebook, and FREE through Kindle Unlimited",
+  ],
+  imageData: adventuresOfLucaAndKaiTheMoonQueenImageData,
+  linkData: adventuresOfLucaAndKaiTheMoonQueenLinkData,
+}
+
+export const celebrationOfTheHistoryOfCelebratingHistoryBookData: BookData = {
+  title: "A Celebration of the History of Celebrating History",
+  descriptions: [
+    "Intended for mature audiences!",
+    "Available in paperback, Ebook, and FREE through Kindle Unlimited",
+  ],
+  imageData: celebrationOfTheHistoryOfCelebratingHistoryImageData,
+  linkData: celebrationOfTheHistoryOfCelebratingHistoryLinkData,
+}
+
+export const books: BookData[] = [
+  adventuresOfLucaAndKaiBook1of3BookData,
+  celebrationOfTheHistoryOfCelebratingHistoryBookData,
+]
