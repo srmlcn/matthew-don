fd52dd43ec756ad64061d38c5f9ad0d085715ab9
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 14:29:39 -0500

    refactor: move the adventures book page props to the book data file

--- src/lib/book-data.ts
+++ src/lib/book-data.ts
@@ -2,6 +2,7 @@ import {
   adventuresOfLucaAndKaiBook2of3ImageData,
   adventuresOfLucaAndKaiBook3of3ImageData,
   adventuresOfLucaAndKaiTheMoonQueenImageData,
+  adventuresOfLucaAndKaiTheMoonQueenImagesData,
   celebrationOfTheHistoryOfCelebratingHistoryImageData,
   celebrationOfTheHistoryOfCelebratingHistoryImagesData,
   type ImageData,
@@ -14,6 +15,7 @@ import {
   type LinkData,
 } from "./link-data"
 import {
+  adventuresOfLucaAndKaiTheMoonQueenReviewsData,
   celebrationOfTheHistoryOfCelebratingHistoryReviewsData,
   ReviewData,
 } from "./review-data"
@@ -35,8 +37,15 @@ export const adventuresOfLucaAndKaiBook1of3BookData: BookData = {
     "Fun for all ages!",
     "Available in paperback, Ebook, and FREE through Kindle Unlimited",
   ],
+  descriptionsExpanded: [
+    "Luca and Kai are two young brothers with an openness for adventure that's as big as their imagination. At least what their imagination can allow while grounded in their room. When two portals open next to them, they are thrust into an experience like no other, meeting unique friends and fearsome enemies along the way. Through fast paced action, hilarious antics and mind-bending physics, our heroes discover they are in control of their own destinies. Who will they decide to become?",
+  ],
+  availability:
+    "Available now in paperback, Ebook, and FREE through Kindle Unlimited!",
   imageData: adventuresOfLucaAndKaiTheMoonQueenImageData,
+  previewImagesData: adventuresOfLucaAndKaiTheMoonQueenImagesData,
   linkData: adventuresOfLucaAndKaiTheMoonQueenLinkData,
+  reviewData: adventuresOfLucaAndKaiTheMoonQueenReviewsData,
 }
 
 export const adventuresOfLucaAndKaiBook2of3BookData: BookData = {
