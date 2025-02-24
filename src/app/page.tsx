80a954e49e8a2cd6c3ee3b5eea0f3fe657ed4244
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 11:53:23 -0500

    refactor: move the coming soon section to a component file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 import { Divider } from "@heroui/react"
-import { Book } from "./components/book"
-import type { BookProps } from "./components/book"
 import { Invitation } from "./components/invitation"
-import {
-  adventuresOfLucaAndKaiBook2of3ImageData,
-  adventuresOfLucaAndKaiBook3of3ImageData,
-} from "@/lib/image-data"
-import {
-  adventuresOfLucaAndKaiBook2of3LinkData,
-  adventuresOfLucaAndKaiBook3of3LinkData,
-} from "@/lib/link-data"
 import type { Metadata } from "next"
 import { Welcome } from "./components/welcome"
 import { Books } from "./components/books"
+import { ComingSoon } from "./components/coming-soon"
 
 export const metadata: Metadata = {
   title: "Home | Matthew Don",
@@ -77,36 +68,6 @@ export default function Home() {
       <Divider orientation="horizontal" />
 
       <Invitation />
-    </div>
-  )
-}
-
-function ComingSoon() {
-  const books: BookProps[] = [
-    {
-      title: "The Adventures of Luca and Kai (Book 2 of 3)",
-      descriptions: ["Coming Soon..."],
-      imageData: adventuresOfLucaAndKaiBook2of3ImageData,
-      linkData: adventuresOfLucaAndKaiBook2of3LinkData,
-    },
-    {
-      title: "The Adventures of Luca and Kai (Book 3 of 3)",
-      descriptions: ["Coming Not Quite As Soon..."],
-      imageData: adventuresOfLucaAndKaiBook3of3ImageData,
-      linkData: adventuresOfLucaAndKaiBook3of3LinkData,
-    },
-  ]
-
-  return (
-    <div className="flex flex-col items-center">
-      <p className="font-bold text-4xl text-center">
-        {"Here's what is coming next!"}
-      </p>
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 py-12">
-        {books.map((book) => (
-          <Book key={book.title} {...book} />
-        ))}
-      </div>
     </div>
   )
 }
