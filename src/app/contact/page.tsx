365294593ad12d0a7d52a20a815703ddacfae56f
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 15:42:48 -0500

    fix: render the profile picture based on the container width on the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,7 +14,7 @@ export default function ContactPage() {
           alt="Profile Picture"
           width={482}
           height={482}
-          className="h-full w-auto"
+          className="w-full h-auto"
         />
         <div className="flex flex-col gap-4">
           <p>
