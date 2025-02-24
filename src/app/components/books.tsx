a1cc9c9471def7ad3666f875a2cf9ea4619913f7
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 14:14:28 -0500

    refactor: move the current books to the book data file

--- src/app/components/books.tsx
+++ src/app/components/books.tsx
@@ -1,36 +1,7 @@
-import { BookData } from "@/lib/book-data"
-import {
-  adventuresOfLucaAndKaiTheMoonQueenImageData,
-  celebrationOfTheHistoryOfCelebratingHistoryImageData,
-} from "@/lib/image-data"
-import {
-  adventuresOfLucaAndKaiTheMoonQueenLinkData,
-  celebrationOfTheHistoryOfCelebratingHistoryLinkData,
-} from "@/lib/link-data"
+import { books } from "@/lib/book-data"
 import { Book } from "./book"
 
 export function Books() {
-  const books: BookData[] = [
-    {
-      title: "The Adventures of Luca and Kai: The Moon Queen (Book 1 of 3)",
-      descriptions: [
-        "Fun for all ages!",
-        "Available in paperback, Ebook, and FREE through Kindle Unlimited",
-      ],
-      imageData: adventuresOfLucaAndKaiTheMoonQueenImageData,
-      linkData: adventuresOfLucaAndKaiTheMoonQueenLinkData,
-    },
-    {
-      title: "A Celebration of the History of Celebrating History",
-      descriptions: [
-        "Intended for mature audiences!",
-        "Available in paperback, Ebook, and FREE through Kindle Unlimited",
-      ],
-      imageData: celebrationOfTheHistoryOfCelebratingHistoryImageData,
-      linkData: celebrationOfTheHistoryOfCelebratingHistoryLinkData,
-    },
-  ]
-
   return (
     <div className="flex flex-col items-center gap-12">
       <p className="font-bold text-4xl text-center">{"Check out my books!"}</p>
