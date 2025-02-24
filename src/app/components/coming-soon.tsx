866d769ad1e65b00d65b3c0c746686ab4e936fa5
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 13:33:22 -0500

    refactor: remove curly braces around static text in the coming soon component

--- src/app/components/coming-soon.tsx
+++ src/app/components/coming-soon.tsx
@@ -27,7 +27,7 @@ export function ComingSoon() {
   return (
     <div className="flex flex-col items-center gap-12">
       <p className="font-bold text-4xl text-center">
-        {"Here's what is coming next!"}
+        Here's what is coming next!
       </p>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
         {books.map((book) => (
