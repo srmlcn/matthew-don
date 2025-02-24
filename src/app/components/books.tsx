b43a3684720cab3b3ca22fcc120b498b5f922753
Spencer Cain <cainspencerm@protonmail.com>
2025-02-24 15:17:32 -0500

    fix: use semantic HTML elements on the books component

--- src/app/components/books.tsx
+++ src/app/components/books.tsx
@@ -3,13 +3,13 @@ import { Book } from "./book"
 
 export function Books() {
   return (
-    <div className="flex flex-col items-center gap-12">
-      <p className="font-bold text-4xl text-center">Check out my books!</p>
+    <section className="flex flex-col items-center gap-12">
+      <h2 className="font-bold text-4xl text-center">Check out my books!</h2>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
         {books.map((book) => (
           <Book key={book.title} {...book} />
         ))}
       </div>
-    </div>
+    </section>
   )
 }
