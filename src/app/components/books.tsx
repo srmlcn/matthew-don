d72d75cae00762e8cb7433a4b6b5b8fcf535ac15
Spencer Cain <cainspencerm@protonmail.com>
2025-02-25 13:33:25 -0500

    feat: use the animated section component on the books component

--- src/app/components/books.tsx
+++ src/app/components/books.tsx
@@ -1,15 +1,16 @@
 import { books } from "@/lib/book-data"
 import { Book } from "./book"
+import { AnimatedSection } from "./animated-section"
 
 export function Books() {
   return (
-    <section className="flex flex-col items-center gap-12">
+    <AnimatedSection className="flex flex-col items-center gap-12">
       <h2 className="font-bold text-4xl text-center">Check out my books!</h2>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
         {books.map((book) => (
           <Book key={book.title} {...book} />
         ))}
       </div>
-    </section>
+    </AnimatedSection>
   )
 }
