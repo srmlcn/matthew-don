abd4da60e58eeaa1709991c33efd3120a399ea48
Spencer Cain <cainspencerm@protonmail.com>
2025-02-25 12:18:45 -0500

    fix: change the about page surrounding html tag to a div

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 export default function AboutPage() {
   return (
-    <section className="flex flex-col items-center gap-8">
+    <div className="flex flex-col items-center gap-8">
       <h1 className="font-bold text-4xl">Like I said, I'm Matt.</h1>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 py-12 items-start place-items-center">
         <Image
@@ -68,6 +68,6 @@ export default function AboutPage() {
           </div>
         </div>
       </div>
-    </section>
+    </div>
   )
 }
