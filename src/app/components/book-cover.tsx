e1a0cf1defd3b2f9d46609d1cfd95c7c5099c956
Spencer Cain <cainspencerm@protonmail.com>
2025-02-25 16:02:16 -0500

    fix: combine className with "h-full" in the book cover component

--- src/app/components/book-cover.tsx
+++ src/app/components/book-cover.tsx
@@ -1,6 +1,7 @@
 import { ImageData } from "@/lib/image-data"
 import { AnimatedSection } from "./animated-section"
 import Image from "next/image"
+import { cn } from "@/lib/utils"
 
 export function BookCover({
   imageData,
@@ -21,11 +22,13 @@ export function BookCover({
     />
   )
 
+  const classNames = cn("h-full", className)
+
   return animate ? (
-    <AnimatedSection animate={animate} className={className}>
+    <AnimatedSection animate={animate} className={classNames}>
       {image}
     </AnimatedSection>
   ) : (
-    <section className={className}>{image}</section>
+    <section className={classNames}>{image}</section>
   )
 }
