e6b3569f505ad294df4a48ded13e6aab7d5e1d5f
Spencer Cain <cainspencerm@protonmail.com>
2025-03-09 10:07:39 -0400

    feat: update the name of the adventures book 2

--- src/lib/book-data.ts
+++ src/lib/book-data.ts
@@ -49,7 +49,7 @@ export const adventuresOfLucaAndKaiBook1of3BookData: BookData = {
 }
 
 export const adventuresOfLucaAndKaiBook2of3BookData: BookData = {
-  title: "The Adventures of Luca and Kai (Book 2 of 3)",
+  title: "The Adventures of Luca and Kai: The Celestial Samurai (Book 2 of 3)",
   descriptions: ["Coming Soon..."],
   imageData: adventuresOfLucaAndKaiBook2of3ImageData,
   linkData: adventuresOfLucaAndKaiBook2of3LinkData,
