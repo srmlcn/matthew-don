6c9d2e2bdd715778aabedc3dec1f21daf4e71863
Spencer Cain <cainspencerm@protonmail.com>
2025-03-10 12:22:26 -0400

    feat: add a category field to the book data type for menu filtering

--- src/lib/book-data.ts
+++ src/lib/book-data.ts
@@ -27,6 +27,7 @@ export type BookData = {
   title: string
   descriptions: string[]
   descriptionsExpanded?: string[]
+  category?: string
   availability?: string
   imageData: ImageData
   linkData: LinkData
@@ -43,6 +44,7 @@ export const adventuresOfLucaAndKaiBook1of3BookData: BookData = {
   descriptionsExpanded: [
     "Luca and Kai are two young brothers with an openness for adventure that's as big as their imagination. At least what their imagination can allow while grounded in their room. When two portals open next to them, they are thrust into an experience like no other, meeting unique friends and fearsome enemies along the way. Through fast paced action, hilarious antics and mind-bending physics, our heroes discover they are in control of their own destinies. Who will they decide to become?",
   ],
+  category: "adventures",
   availability:
     "Available now in paperback, Ebook, and FREE through Kindle Unlimited!",
   imageData: adventuresOfLucaAndKaiTheMoonQueenImageData,
@@ -60,6 +62,7 @@ export const adventuresOfLucaAndKaiTheComicsBookData: BookData = {
   descriptionsExpanded: [
     "Join Luca and Kai on their adventures in a brand new way! Featuring original artwork by Christopher Don, travel the cosmos through the beautiful, fully colored panels of Adventures of Luca and Kai: The Comics!",
   ],
+  category: "adventures",
   availability:
     "Available now in paperback, Ebook, and FREE through Kindle Unlimited!",
   imageData: adventuresOfLucaAndKaiTheComicsImageData,
@@ -71,6 +74,7 @@ export const adventuresOfLucaAndKaiTheComicsBookData: BookData = {
 export const adventuresOfLucaAndKaiBook2of3BookData: BookData = {
   title: "The Adventures of Luca and Kai: The Celestial Samurai (Book 2 of 3)",
   descriptions: ["Coming 2025"],
+  category: "adventures",
   imageData: adventuresOfLucaAndKaiBook2of3ImageData,
   linkData: adventuresOfLucaAndKaiBook2of3LinkData,
 }
@@ -78,6 +82,7 @@ export const adventuresOfLucaAndKaiBook2of3BookData: BookData = {
 export const adventuresOfLucaAndKaiBook3of3BookData: BookData = {
   title: "The Adventures of Luca and Kai (Book 3 of 3)",
   descriptions: ["Coming Later..."],
+  category: "adventures",
   imageData: adventuresOfLucaAndKaiBook3of3ImageData,
   linkData: adventuresOfLucaAndKaiBook3of3LinkData,
 }
@@ -92,6 +97,7 @@ export const celebrationOfTheHistoryOfCelebratingHistoryBookData: BookData = {
     "PLEASE NOTE this book is purely a work of fiction, and does not contain any historical accuracy. FURTHERMORE, this book is intended for adults only. Yes, the humor is immature, but on top of terrible language and some cringeworthy attempts at jokes, the story also delves into serious and potentially triggering topics like depression and suicide.",
     "We all love spending a day off work and eating obscene amounts of food on our favorite holidays, but do you know the stories behind these special days? Join me as we take a journey through history and learn how our traditions came to be. Your mind will swell with knowledge as you learn things like how the customs of Halloween spur from a despicable dentist looking to make money by rotting the teeth of children, or how Hanukkah celebrates the unexpectedly decent career of Adam Sandler. You will bask in glorious wisdom while you discover how the Romans celebrated the new year by having drunken orgies, and how Cinco de Mayo was born thanks to hot dogs and an inept French cook. You will learn all this and more within the pages of this informational text, A Celebration of the History of Celebrating History.",
   ],
+  category: "others",
   availability:
     "Available now in paperback, Ebook, and FREE through Kindle Unlimited!",
   imageData: celebrationOfTheHistoryOfCelebratingHistoryImageData,
@@ -102,6 +108,7 @@ export const celebrationOfTheHistoryOfCelebratingHistoryBookData: BookData = {
 
 export const books: BookData[] = [
   adventuresOfLucaAndKaiBook1of3BookData,
+  adventuresOfLucaAndKaiTheComicsBookData,
   celebrationOfTheHistoryOfCelebratingHistoryBookData,
 ]
 
