2efccc14a7cfe030d364cc381464f7013a8fae22
Spencer Cain <cainspencerm@protonmail.com>
2025-03-10 15:26:10 -0400

    fix: set the h2 text color to zinc-300 in dark mode on the navbar

--- src/app/components/site-navbar.tsx
+++ src/app/components/site-navbar.tsx
@@ -95,7 +95,7 @@ export function SiteNavbar() {
 
         <span />
 
-        <h2 className="text-sm font-semibold text-zinc-500">
+        <h2 className="text-sm font-semibold light:text-zinc-500 dark:text-zinc-300">
           The Adventures of Luca and Kai
         </h2>
         <FilteredMenuItems
@@ -105,7 +105,9 @@ export function SiteNavbar() {
 
         <span />
 
-        <h2 className="text-sm font-semibold text-zinc-500">Others</h2>
+        <h2 className="text-sm font-semibold light:text-zinc-500 dark:text-zinc-300">
+          Others
+        </h2>
         <FilteredMenuItems
           filter={BOOK_CATEGORIES.OTHERS}
           onSelect={() => setIsMenuOpen(false)}
