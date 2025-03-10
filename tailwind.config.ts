c2154289a3e6a98c909fed27f26ab4d17bda962d
Spencer Cain <cainspencerm@protonmail.com>
2025-03-10 14:59:12 -0400

    fix: set the primary color in dark mode for heroui using hex format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,7 +30,7 @@ export default {
         },
         dark: {
           colors: {
-            primary: "var(--foreground)",
+            primary: "#ffffff",
             secondary: "#00ff77",
           },
         },
