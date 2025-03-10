fa8e06b03ac9806874cba9df048cfa64b69d50c9
Spencer Cain <cainspencerm@protonmail.com>
2025-03-10 13:37:54 -0400

    feat: default to the light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="transition-colors duration-1000 ease-in-out">
+    <html
+      lang="en"
+      className="light transition-colors duration-1000 ease-in-out"
+    >
       <body className={`antialiased min-h-screen flex flex-col`}>
         <div className="flex-1">
           <Providers>
