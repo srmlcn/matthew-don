0775912d273a7e209a284f734bedcd18d210ef8f
Spencer Cain <cainspencerm@protonmail.com>
2025-08-23 15:32:38 -0400

    feat: add the confetti component to the NewRelease component

--- src/app/components/new-release.tsx
+++ src/app/components/new-release.tsx
@@ -1,10 +1,12 @@
 import { AnimatedSection } from "@/app/components/animated-section"
-import { Book } from "./book"
 import type { BookData } from "@/lib/book-data"
+import { Book } from "./book"
+import { Confetti } from "@/app/components/confetti"
 
 export function NewRelease({ bookData }: { bookData: BookData }) {
   return (
     <AnimatedSection className="flex flex-col items-center gap-12">
+      <Confetti total={99} />
       <h2 className="font-bold text-4xl text-center">My latest release!</h2>
       <Book size="lg" {...bookData} />
     </AnimatedSection>
